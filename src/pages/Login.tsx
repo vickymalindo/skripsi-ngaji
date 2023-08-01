a1532f4f3dd5f916ef6acd43b46257a64f391caf
vickymalindo <vicky.malindo@gmail.com>
2023-08-01 12:16:31 +0700

    add _redirects

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -17,40 +17,40 @@ const Login = () => {
 
   const handleLogin = async () => {
     setIsLoading((prev) => !prev);
-    setTimeout(async () => {
-      try {
-        const login = await axios.post(
-          import.meta.env.VITE_BASE_URL + 'auth/login',
-          {
-            username,
-            password,
-          }
-        );
-
-        const { access_token } = login.data;
-        localStorage.setItem('token', encrypt(access_token));
-        const user = await axios({
-          method: 'POST',
-          url: import.meta.env.VITE_BASE_URL + 'auth/me',
-          headers: { Authorization: `Bearer${access_token}` },
-        });
-
-        localStorage.setItem('data', encrypt(user.data));
-
-        const { level } = user.data;
-        setIsLoading((prev) => !prev);
-        if (level === 'admin') {
-          navigate('/admin/list/teacher');
-        } else if (level === 'guru') {
-          navigate('/teacher/students');
-        } else {
-          navigate('/parent/rote');
+    // setTimeout(async () => {
+    try {
+      const login = await axios.post(
+        import.meta.env.VITE_BASE_URL + 'auth/login',
+        {
+          username,
+          password,
         }
-      } catch (error) {
-        setIsError('Password / username salah');
-        setIsLoading((prev) => !prev);
+      );
+
+      const { access_token } = login.data;
+      localStorage.setItem('token', encrypt(access_token));
+      const user = await axios({
+        method: 'POST',
+        url: import.meta.env.VITE_BASE_URL + 'auth/me',
+        headers: { Authorization: `Bearer${access_token}` },
+      });
+
+      localStorage.setItem('data', encrypt(user.data));
+
+      const { level } = user.data;
+      setIsLoading((prev) => !prev);
+      if (level === 'admin') {
+        navigate('/admin/list/teacher');
+      } else if (level === 'guru') {
+        navigate('/teacher/students');
+      } else {
+        navigate('/parent/rote');
       }
-    }, 80000);
+    } catch (error) {
+      setIsError('Password / username salah');
+      setIsLoading((prev) => !prev);
+    }
+    // }, 80000);
   };
 
   return (
